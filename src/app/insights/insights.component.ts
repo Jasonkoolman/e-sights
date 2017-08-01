0e4007eee69522ac022a87fe21d5ab83f809f782
Jason Koolman <jason.koolman@incentro.com>
2017-08-01 16:14:54 +0200

    Detect changes on 'data' property

--- src/app/insights/insights.component.ts
+++ src/app/insights/insights.component.ts
@@ -7,16 +7,28 @@ import Chart from 'chart.js';
   styleUrls: ['./insights.component.scss']
 })
 export class InsightsComponent implements OnChanges {
-  @Input() data;
+  @Input() data: Object|InsightsData;
 
   constructor() { }
 
+  /**
+   * Detect whether a data-bound property changes.
+   *
+   * @param changes
+   */
   ngOnChanges(changes: any) {
-
     if (changes.hasOwnProperty('data') && changes.data.currentValue ) {
-      console.log('GOT DATA') ;
-
+      console.log('Data retrieved: ', this.data);
+      this.data = new InsightsData(this.data);
+      this.load();
     }
+  }
+
+  /**
+   * Load the insights data.
+   */
+  load() {
+    console.log('Insights data: ', this.data);
   }
 
 }
